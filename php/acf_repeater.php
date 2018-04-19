12d1088d4b2eb439391e6aea0f520c40e4de91aa
iakut <iakut.pd@gmail.com>
2018-04-19 10:15:35 +0300

    update before merge

--- php/acf_repeater.php
+++ php/acf_repeater.php
@@ -1,7 +1,7 @@
 <?php
 	if( have_rows('parent_var') ) { 
 		while( have_rows('parent_var') ) : the_row();
-			$child_var = get_sub_field('child_var'); // field
+			$child_var = get_sub_field('child_var'); // field 
 		endwhile;
 	}
 ?>
