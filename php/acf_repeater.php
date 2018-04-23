5eb073358387cbddaaf6b113fba411df23c537cd
iakut <iakut.pd@gmail.com>
2018-04-23 14:30:44 +0300

    Upadte test

--- php/acf_repeater.php
+++ php/acf_repeater.php
@@ -1,13 +1,7 @@
 <?php
 	if( have_rows('parent_var') ) { 
 		while( have_rows('parent_var') ) : the_row();
-<<<<<<< HEAD
-			$child_var = get_sub_field('child_var'); // field 
-||||||| merged common ancestors
 			$child_var = get_sub_field('child_var'); // field
-=======
-			$child_var = get_sub_field('child_var');
->>>>>>> 2eabfeb714f699ed497ea69df7f97ea68f3bfb6a
 		endwhile;
 	}
 ?>
