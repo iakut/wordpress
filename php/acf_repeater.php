debb9101a95b9309c1dec4ef93f6a16bb2ef6d9f
Andrei Ciubara <iakut.pd@gmail.com>
2018-04-23 14:16:52 +0300

    Update acf_repeater.php

--- php/acf_repeater.php
+++ php/acf_repeater.php
@@ -1,11 +1,7 @@
 <?php
 	if( have_rows('parent_var') ) { 
 		while( have_rows('parent_var') ) : the_row();
-<<<<<<< HEAD
-			$child_var = get_sub_field('child_var'); // field 
-=======
 			$child_var = get_sub_field('child_var');
->>>>>>> 5a5fccea2a21d7f93951f04d89c4ec7badada069
 		endwhile;
 	}
 ?>
